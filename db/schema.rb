26b389b290917a248555e2c68714c3200300bf14
Andrew Flagstead <amf0212@gmail.com>
2021-03-12 14:28:52 -0800

    added user model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_12_222308) do
+
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
+    t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer "user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
