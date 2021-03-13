e39b1633bcca1e1af73579ba63135f7f5acbd98f
Andrew Flagstead <amf0212@gmail.com>
2021-03-13 10:17:15 -0800

    added show for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravitar_for(user, options = { size: 80 })
+    size = options[:size]
+    hash = Digest::MD5.hexdigest(user.email)
+    url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(url, alt: user.email, class: "rounded mx-auto d-block shadow mt-3")
+  end
 end
