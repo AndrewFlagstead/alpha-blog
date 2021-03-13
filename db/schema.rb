f9949c34fe2d54bb261eac92aa580d4cb7c80d91
Andrew Flagstead <amf0212@gmail.com>
2021-03-12 16:35:39 -0800

    added one to many from users to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_222308) do
+ActiveRecord::Schema.define(version: 2021_03_13_000904) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
