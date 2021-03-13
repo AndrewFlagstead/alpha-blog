c1a88a4e26786b1914d67c877d58a44d22dc8da7
Andrew Flagstead <amf0212@gmail.com>
2021-03-13 15:56:48 -0800

    added backend logic for logging in / out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Successful Log In"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid username/password combination"
+      render 'new'
+    end
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
